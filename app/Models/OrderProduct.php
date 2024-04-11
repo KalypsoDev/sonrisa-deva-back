df03476bb1e0ead3b92d51c622455dbb377d407e
Desiree <desireesanchezdev@gmail.com>
2024-04-11 16:13:52 +0200

    Create relations on OrderProduct Model

---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderProduct extends Model
+{
+    use HasFactory;
+
+    protected $guarded =  [];
+
+    public function orders()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
